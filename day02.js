7c34b33fe1b7f015dd7026d76a26327f0e0557b1
laiba <laibaazhar135@gmmail.com>
2026-09-25 13:12:53 +0500

    day 2: Conditional statements

--- day02.js
+++ day02.js
@@ -1,17 +1,67 @@
-const readline = require('readline/promises');
-const {stdin:input ,stdout:output}=require('process');
+//Practice for learning the readline with async/await for user input...
 
-async function main(params) {
+// const readline = require('readline/promises');
+// const {stdin:input ,stdout:output}=require('process');
+
+// async function main(params) {
+//     const rl = readline.createInterface({input,output});
+
+//     const role =await rl.question('Enter your role: (vip/member/guest): ');
+//     const priceInput = await rl.question('enter cart price: ');
+
+//    const price = Number(priceInput);
+
+//    console.log(`Price : ${price} and role: ${role}`);
+
+//    rl.close();
+// }
+
+// main();
+
+//-------------------------------------------------------------------------
+
+
+const readline= require('readline/promises');
+const {stdin:input ,stdout:output}= require('process');
+
+async function main() {
     const rl = readline.createInterface({input,output});
 
-    const role =await rl.question('Enter your role: (vip/member/guest): ');
-    const priceInput = await rl.question('enter cart price: ');
+    const role =await rl.question('Enter your role(VIP,MEMBER,GUEST): ');
+    const priceInput= await rl.question('Enter the price of product: ');
+    if(priceInput.trim()===''){
+        console.log('Error, Price cannot be empty...');
+        rl.close();
+        return;
+    }
 
-   const price = Number(priceInput);
+    const price = Number(priceInput);
+    let discount;
 
-   console.log(`Price : ${price} and role: ${role}`);
+    switch(role.toLowerCase()){
+        case 'vip':
+            discount=0.20;
+            break;
+        case 'member':    
+        discount=price>=100?0.10:0.05;
+        break;
+        case 'guest':
+            discount=0;
+            break;
+            default:
+                console.log('User role is unrecognized');
+                rl.close();
+                return;
+    }
+    let discountedPrice=price-(price*discount);
 
-   rl.close();
+    const freeShipping=discountedPrice>=80?true:false;
+    let FS= freeShipping? 'available':'not-available';
+//also could be const freeShipping = discountedPrice>=80?'available:not-available;
+
+    console.log(`Calculated discount: ${discount*100} % \nFinal Price: ${discountedPrice}\nFree shipping: ${FS}`);
+
+    rl.close();
 }
 
 main();
